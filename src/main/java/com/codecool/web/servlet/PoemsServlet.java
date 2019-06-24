75a419ffed70cdaf31e24693edeb8481aa70faf3
farkaspeti <farkas.peti97@gmail.com>
2019-06-24 16:54:45 +0200

    Poem/Poems servlets implemented

--- src/main/java/com/codecool/web/servlet/PoemsServlet.java
+++ src/main/java/com/codecool/web/servlet/PoemsServlet.java
@@ -2,9 +2,10 @@ package com.codecool.web.servlet;
 
 import com.codecool.web.dao.PoemDao;
 import com.codecool.web.dao.database.DatabasePoemDao;
-import com.codecool.web.model.Shop;
+import com.codecool.web.model.Poem;
+import com.codecool.web.service.PoemService;
 import com.codecool.web.service.exception.ServiceException;
-import com.codecool.web.service.simple.SimpleShopService;
+import com.codecool.web.service.simple.SimplePoemService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,25 +14,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
-@WebServlet("/protected/shop")
-public final class ShopServlet extends AbstractServlet {
+@WebServlet("/protected/poems")
+public final class PoemsServlet extends AbstractServlet {
+    
+    // https://www.postgresql.org/docs/current/static/errcodes-appendix.html
+    private static final String SQL_ERROR_CODE_UNIQUE_VIOLATION = "23505";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
             PoemDao poemDao = new DatabasePoemDao(connection);
-            ShopService shopService = new SimpleShopService(poemDao);
-
+            PoemService poemService = new SimplePoemService(poemDao);
+    
             String id = req.getParameter("id");
 
-            Shop shop = shopService.getShop(id);
+            List<Poem> poems = poemService.getPoemsByUserId(id);
 
-            sendMessage(resp, HttpServletResponse.SC_OK, shop);
-        } catch (ServiceException ex) {
-            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            sendMessage(resp, HttpServletResponse.SC_OK, poems);
         } catch (SQLException ex) {
             handleSqlError(resp, ex);
+        } catch (ServiceException ex) {
+            sendMessage(resp, HttpServletResponse.SC_OK, ex.getMessage());
         }
     }
 }
